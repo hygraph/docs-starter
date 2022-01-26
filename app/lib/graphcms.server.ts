d5c34e051b2a1853d201f4b291f144749ae10e7b
Jamie Barton <jamie@notrab.dev>
2022-01-26 19:34:56 +0000

    feat: graphql codegen (#1)
    Co-authored-by: João Pedro Schmitz <jpedroschmitz@hotmail.com>

---
+++ app/lib/graphcms.server.ts
@@ -0,0 +1,5 @@
+import { GraphQLClient } from "graphql-request";
+
+export const graphcms = new GraphQLClient(
+  process.env.GRAPHCMS_ENDPOINT as string
+);
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,23 @@
+import { useLoaderData } from "remix";
+import type { LoaderFunction } from "remix";
+
+import { graphcms } from "~/lib/graphcms.server";
+import { getSdk } from "~/generated/schema.server";
+import type { GetAllNavItemsQuery } from "~/generated/schema.server";
+
+export const loader: LoaderFunction = async () => {
+  const { GetAllNavItems } = getSdk(graphcms);
+
+  return await GetAllNavItems();
+};
+
 export default function Index() {
+  const data = useLoaderData<GetAllNavItemsQuery>();
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+    <div>
       <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
 }
