ad437f7b185054facc275c615348ae2cc5c320e2
Jamie Barton <jamie@notrab.dev>
2022-01-26 17:54:21 +0000

    feat: initial codegen config

---
+++ app/lib/graphcms.server.ts
@@ -0,0 +1,5 @@
+import { GraphQLClient } from "graphql-request";
+
+export const graphcms = new GraphQLClient(
+  "https://api-eu-central-1.graphcms.com/v2/ckyvt6qiu0k6101z09eyzh92t/master"
+);
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,24 @@
+import { useLoaderData } from "remix";
+import type { LoaderFunction } from "remix";
+
+import { graphcms } from "~/lib/graphcms.server";
+import { getSdk } from "~/generated/schema.server";
+
+export const loader: LoaderFunction = async () => {
+  const { GetAllNavItems } = getSdk(graphcms);
+
+  const { navItems } = await GetAllNavItems();
+
+  return { navItems };
+};
+
 export default function Index() {
+  const data = useLoaderData();
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+    <div>
       <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
 }
