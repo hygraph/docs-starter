986f5a3ef8462914536d2ec7d7db7ab1ccf4724d
João Pedro Schmitz <jpedroschmitz@hotmail.com>
2022-02-07 14:57:19 -0300

    close menu on route change

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,27 +1,37 @@
-import { Link } from "remix";
+import { useEffect, useRef } from "react";
+import { Link, useLocation } from "remix";
 import { Popover } from "@headlessui/react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 import cc from "classcat";
 
-import { GraphCMSLogo } from "./graphcms-logo";
+import { GraphCMSLogo } from "~/components/graphcms-logo";
 import { GetAllNavItemsQuery } from "~/generated/schema.server";
-import { Nav, NavRenderer } from "./nav";
+import { Nav } from "~/components/nav";
 
 export function Header({ navigations }: GetAllNavItemsQuery) {
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const location = useLocation();
+
+  useEffect(() => {
+    // Dirty workaround to close the nav when the route changes
+    if (buttonRef.current) buttonRef.current.click();
+  }, [location]);
+
   return (
     <div className="sticky top-0 z-50">
       <Popover
         as="header"
         className={({ open }) =>
           cc([
-            open ? "fixed inset-0 z-40 overflow-y-auto bg-white" : "",
-            "mx-auto max-w-7xl",
+            open
+              ? "fixed inset-0 z-40 overflow-y-auto bg-white"
+              : "bg-blue-700",
           ])
         }
       >
-        {({ open }) => (
+        {({ open, close }) => (
           <>
-            <div className="flex items-center justify-between bg-blue-700 px-6 py-4 md:px-12 md:py-5">
+            <div className="mx-auto flex max-w-7xl items-center justify-between bg-blue-700 px-6 py-4 md:px-12 md:py-5">
               <Link to="/" className="block w-full">
                 <GraphCMSLogo className="inline-block max-w-[140px] text-white" />
               </Link>
@@ -37,6 +47,15 @@ export function Header({ navigations }: GetAllNavItemsQuery) {
                 </Popover.Button>
               </div>
             </div>
+
+            <button
+              ref={buttonRef}
+              className="hidden"
+              aria-hidden="true"
+              onClick={() => close()}
+            >
+              close
+            </button>
 
             <Popover.Panel as="nav" className="lg:hidden" aria-label="Global">
               <div className="mx-auto max-w-3xl px-4 py-6">
