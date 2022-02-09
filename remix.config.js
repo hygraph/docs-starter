e3c304fbb9f51961e6e70205cdf3bf2062ee4265
João Pedro Schmitz <jpedroschmitz@hotmail.com>
2022-02-09 20:32:03 -0300

    chore: change deployment to vercel (#5)

--- remix.config.js
+++ remix.config.js
@@ -5,7 +5,6 @@ module.exports = {
   appDirectory: 'app',
   assetsBuildDirectory: 'public/build',
   publicPath: '/build/',
-  serverBuildDirectory: 'build',
-  devServerPort: 8002,
+  serverBuildDirectory: 'api/_build',
   ignoredRouteFiles: ['.*'],
 };
